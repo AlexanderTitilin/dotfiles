6c7966038e63caa60293bc4c97b6616e52b3dee5
Alexander Titilin <lambdadelta89@gmail.com>
2026-04-09 18:34:22 +0300

    add plugins

---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,10 @@
+return {
+    { import = "plugins.tabby" },
+    { import = "plugins.dadboard" },
+    { import = "plugins.nvim-tree" },
+    { import = "plugins.neogit" },
+    { import = "plugins.cursorline" },
+    { import = "plugins.terminal" },
+    { import = "plugins.colorscheme" },
+    { import = "plugins.mkdir" },
+}
